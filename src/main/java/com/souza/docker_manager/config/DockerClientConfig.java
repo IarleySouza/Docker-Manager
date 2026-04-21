4a76fab7fdebad92912eb3b6628814c68c1074bb
Iarley Souza <iarleysouza615@gmail.com>
2026-04-21 12:09:39 -0300

    add coding

---
+++ src/main/java/com/souza/docker_manager/config/DockerClientConfig.java
@@ -0,0 +1,51 @@
+package com.souza.docker_manager.config;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.RemoteApiVersion;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class DockerClientConfig {
+
+    @Value("${docker.socket.path:unix:var/run/docker.sock}")
+    private String dockerSocketPath;
+
+    @Bean
+    public DockerClient buildDockerClient() {
+        DefaultDockerClientConfig.Builder dockerClientConfigBuilder = DefaultDockerClientConfig.createDefaultConfigBuilder();
+
+        assert this.dockerSocketPath != null;
+        if (this.dockerSocketPath.startsWith("unix://")) {
+            dockerClientConfigBuilder.withDockerHost(this.dockerSocketPath)
+                    .withApiVersion(RemoteApiVersion.VERSION_1_44)
+                    .withDockerTlsVerify(false);
+        }
+
+        DefaultDockerClientConfig dockerClientConfig = dockerClientConfigBuilder.build();
+
+        ApacheDockerHttpClient dockerHttpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(dockerClientConfig.getDockerHost())
+                .maxConnections(5)
+                .connectionTimeout(Duration.ofMillis(300))
+                .responseTimeout(Duration.ofSeconds(3))
+                .build();
+
+
+        DockerClient client =  DockerClientBuilder.getInstance(dockerClientConfig)
+                .withDockerHttpClient(dockerHttpClient)
+                .build();
+        
+        client.pingCmd().exec();
+
+        return client;
+
+
+    }
+}
---
+++ src/main/java/com/souza/docker_manager/controllers/DockerContainersController.java
@@ -0,0 +1,57 @@
+package com.souza.docker_manager.controllers;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.souza.docker_manager.service.DockerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/containers")
+@Slf4j
+public class DockerContainersController {
+
+    private final DockerService dockerService;
+
+    @Autowired
+    public DockerContainersController(DockerService dockerService) {
+        this.dockerService = dockerService;
+    }
+
+    @GetMapping
+    public List<Container> listContainers(@RequestParam(required = false) Boolean all) {
+        log.info("Listing containers with all={}", all.toString());
+        return dockerService.listContainers(all);
+
+    }
+
+    @PostMapping(value = "/{id}/start")
+    public ResponseEntity<String> startContainer(@PathVariable String id) {
+        try {
+            dockerService.startContainer(id);
+            log.info("Container {} started.", id);
+            return new ResponseEntity<>( "Container " + id + "started", HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Failed to start container: " + e.getMessage());
+        }
+    }
+    @PostMapping(value = "/{id}/stop")
+    public ResponseEntity<String> stopContainer(@PathVariable String id) {
+        dockerService.stopContainer(id);
+        return ResponseEntity.ok("Container " + id + " stopped successfully.");
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteContainer(@PathVariable String id) {
+        dockerService.deleteContainer(id);
+    }
+    @PostMapping("/create")
+    public void createContainer(@RequestBody String container) {
+        dockerService.createContainer(container);
+    }
+}
